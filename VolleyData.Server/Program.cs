6fbb0038f3689dbcd61f0fc0bfd731a072bbb08a
Jochen Gast <jochen.gast@outlook.de>
2023-10-24 08:19:04 +0200

    feat: Deliver WebAssembly on calling server address

--- VolleyData.Server/Program.cs
+++ VolleyData.Server/Program.cs
@@ -33,18 +33,18 @@ builder.Services.AddControllers();
 //builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseWebAssemblyDebugging();
 }
 
 app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
 app.UseCors(_corsPolicy);
 app.UseRouting();
 app.UseGrpcWeb();
@@ -52,7 +52,7 @@ app.UseAuthorization();
 app.MapControllers();
 app.MapGrpcService<ToDoService>().EnableGrpcWeb();
 app.MapGrpcService<TimeService>().EnableGrpcWeb();
+app.MapFallbackToFile("index.html");
 
 
-
-app.Run();
+app.Run("https://localhost:7298");
